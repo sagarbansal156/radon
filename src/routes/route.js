b237535a9d3fd1c640011492d0a272728e27d003
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-02 18:17:37 +0530

    final

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const externalModule = require('./logger')
-
+const logger = require('../logger/logger')
 const router = express.Router();
-
+const Date1 = require('../util/helper')
+const Month1 = require('../util/helper')
+const Batchinfo = require('../util/helper')
+const Flatter1 = require('../validator/formattor')
+const Flatter2 = require('../validator/formattor')
+const Flatter3 = require('../validator/formattor')
 router.get('/test-me', function (req, res) {
     console.log('The constant in logger route has a value '+externalModule.endpoint)
     console.log('The current batch is '+externalModule.batch)
     externalModule.log()
+    logger.welcome()
+    Date1.PrintDate()
+    Month1.PrintMonth()
+    Batchinfo.getbatchinfo()
+    Flatter1.Trim()
+    Flatter2.lowercase()
+    Flatter3.uppercase()
+
+
     res.send('My first ever api!')
 });
 
