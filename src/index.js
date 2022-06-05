86ac5b0a6dc9313819e1fd8fd7261f749cf332ad
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-06 00:06:55 +0530

    yes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh87698-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://SagarBansal:HmMiUeU0MyktjG49@cluster0.3yhyk.mongodb.net/SagarBansal", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
