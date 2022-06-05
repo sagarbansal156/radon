02caef0a88149764b1674f319628f52562cf4bdd
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-05 15:19:26 +0530

    hurray

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,20 +1,27 @@
 const express = require('express');
 
 const router = express.Router();
-// question -1
+// Question -1
 router.get('/test-me', function (req, res) {
      let arr =['Ramayana','Mahabharata','Amrit','Sholay']
         res.send('list of the movies that i like are'+ ' '+ arr+'.')
 });
-
+// Question -2
 router.get('/movies/:indexNumber', function (req, res) {  
     let arr =['Ramayana','Mahabharata','Amrit','Sholay']
-    console.log(arr[0])
-    let item = arr[req.params.indexNumber]
-     console.log(item)
-      res.send(item)
+        let item = arr[req.params.indexNumber]
+           res.send(item);
 });
-
+// Question -3 I have changed the movies  handler to movie so that i can run Q2 and Q3 seprately
+router.get('/movie/:indexNumber', function (req, res) {  
+  let arr =['Ramayana','Mahabharata','Amrit','Sholay']
+      let item = arr[req.params.indexNumber]
+      if(req.params.indexNumber<=3){
+         res.send(item);}
+         else (
+           res.send(" Error Please use a valid index number")
+         )
+});
 //Assignment 2 by Pritesh Sir
 router.post('/test-me4', function (req, res) {
    let player = [{
