99501fbdd3cdd57d506f975253b3ff49e5c9a710
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-06 07:29:36 +0530

    yeh

--- src/routes/route.js
+++ src/routes/route.js
@@ -18,7 +18,7 @@ const router = express.Router();
          "sports": [
          "cricket"]
          },
-         {"name": "kall0",
+         {"name": "kallo",
          "dob": "1/08/2000",
          "gender": "female",
          "city": "ghaziabad",
@@ -33,27 +33,19 @@ const router = express.Router();
     
     let newPlayer = req.body
     let newPlayersName = newPlayer.name
-    let isNameRepeated = false
-
-    //let player = players.find(p => p.name == newPlayersName)
+    
+    
 
     for(let i = 0; i < players.length; i++) {
         if(players[i].name == newPlayersName) {
-            isNameRepeated = true;
-            break;
-        }
+          res.send("This player was already added!")
+             }
+        else {
+          players.push(newPlayer)
+          res.send(players)
+      }
     }
-
-    //undefined is same as false/ a falsy value
-    if (isNameRepeated) {
-        //Player exists
-        res.send("This player was already added!")
-    } else {
-        //New entry
-        players.push(newPlayer)
-        res.send(players)
-    }
-})
+    })
   
 
 module.exports = router;
