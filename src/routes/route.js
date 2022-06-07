8c37ca7c7fea67277a4ceb3ff8bf0a4da38cbc5b
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-07 22:59:26 +0530

    doit

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,26 +1,17 @@
 const express = require('express');
 const router = express.Router();
-// const UserModel= require("../models/userModel.js")
-const UserController = require("../controllers/userController")
+
+
 const BookController = require("../controllers/bookController")
-
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
-
-router.post("/createUser", UserController.createUser)
-
-router.get("/getUsersData", UserController.getUsersData)
-
+// Question no-1
 router.post("/createBook", BookController.createBook)
 
-router.get("/getBooksData", BookController.getBooksData)
 //question no-2
 router.get("/bookList", BookController.bookList)
 // question no -3
 router.post("/getBooksInYear/", BookController.getBooksInYear)
 // Question no -4
-router.get("/getParticularBooks/", BookController.getParticularBooks)
+router.post("/getParticularBooks/", BookController.getParticularBooks)
 //Question no -5
 router.get("/getXINRBooks", BookController.getXINRBooks)
 //Qestion no-6 
