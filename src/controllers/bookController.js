cf05f6e055f04411eaa35e74d08596efc915c483
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-07 12:00:05 +0530

    all1

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -3,7 +3,6 @@ const BookModel = require("../models/bookModel")
 
 const createBook = async function (req, res) {
     let data = req.body
-
     let savedData = await BookModel.create(data)
     res.send({ msg: savedData })
 }
