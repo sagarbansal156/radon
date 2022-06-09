5502d6b74c5ff3ec680115d614a9841cec96a280
sagarbansal156 <sagarbansal156@gmail.com>
2022-06-09 22:59:32 +0530

    wellDone

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -8,26 +8,23 @@ const createBook = async function (req, res) {
     let book = req.body
     if(!book.author){res.send("Please enter the author details")}
     let x = book.author
-    let authorPresent = await authorModel.findByID(x) 
-    if(!authorPresent){res.send("author detail is not valid")}
+    let authorPresent = await authorModel.findById(x)
+    if(authorPresent==null){res.send("author detail is not valid")}
 
 
     if(!book.publisher){res.send("Please enter the publisher details")}
-    let publisherPresent= await publisherModel.findByID(book.publisher)
-    if(!publisherPresent){res.send("publisher detals is not valid")}
+    let publisherPresent= await publisherModel.findById(book.publisher)
+    if(publisherPresent==null){res.send("publisher detals is not valid")}
 
     let bookCreated = await bookModel.create(book)
     res.send({ data: bookCreated })
     
 }
 
-const getBooksData = async function (req, res) {
-    let books = await bookModel.find()
-    res.send({ data: books })
-}
+
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+    let specificBook = await bookModel.find().populate('author')
     res.send({ data: specificBook })
 
 }
@@ -35,5 +32,4 @@ const getBooksWithAuthorDetails = async function (req, res) {
 
 
 module.exports.createBook = createBook
-module.exports.getBooksData = getBooksData
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
--- src/routes/route.js
+++ src/routes/route.js
@@ -4,17 +4,14 @@ const router = express.Router();
 const authorController= require("../controllers/authorController")
 const bookController= require("../controllers/bookController")
 const publisherController = require("../controllers/publisherController")
-router.get("/test-me", function (req, res) {
-    res.send("My first ever api!")
-})
+
+
 
 router.post("/createAuthor", authorController.createAuthor  )
 
 router.post("/createPublisher", publisherController.createPublisher)
 
 router.post("/createBook", bookController.createBook  )
-
-router.get("/getBooksData", bookController.getBooksData)
 
 router.get("/getBooksWithAuthorDetails", bookController.getBooksWithAuthorDetails)
 
